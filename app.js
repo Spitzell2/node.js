32678647ab5db6fa5660be0826fe43758727261a
Spitzell2 <115994312+Spitzell2@users.noreply.github.com>
2023-10-29 22:17:12 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = 3000; // El puerto en el que se ejecutará el servidor
+
+const cors = require('cors'); // Requiere la biblioteca cors
+
+// Habilita CORS para todas las rutas
+app.use(cors());
+
+// Configuración de pg-promise para conectarse a PostgreSQL
+const pgp = require('pg-promise')();
+const connectionString = 'postgres://postgres:Fbc*gde3EDA1DCCE54*BFa5AGD4bc3**@viaduct.proxy.rlwy.net:12135/railway'; // Reemplaza 'DATABASE_URL' con tu URL de la base de datos
+
+const db = pgp(connectionString);
+
+// Ruta de ejemplo para realizar una consulta a la base de datos
+app.get('/consultar', async (req, res) => {
+    try {
+        // Realiza una consulta a la base de datos
+        const result = await db.any('SELECT * FROM mytable');
+        res.json(result);
+    } catch (error) {
+        console.error('Error al realizar la consulta:', error);
+        res.status(500).json({ error: 'Ocurrió un error al consultar la base de datos' });
+    }
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+    console.log(`Servidor en funcionamiento en http://localhost:${port}`);
+});
